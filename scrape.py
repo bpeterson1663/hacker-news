975001c4594e3b75d631335145aab21a1992c094
Brady Peterson <bpeterson1663@gmail.com>
2024-01-10 22:19:59 -0600

    feat(scrape): finish scraper

--- scrape.py
+++ scrape.py
@@ -2,11 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
 
-res = requests.get("https://news.ycombinator.com/")
+ADDITIONAL_PAGES = 3
 
+res = requests.get("https://news.ycombinator.com/?p=1")
 soup = BeautifulSoup(res.text, "html.parser")
 links = soup.select(".titleline")
 subtext = soup.select(".subtext")
+
+for page_number in range(2, ADDITIONAL_PAGES + 1):
+    page_res = requests.get("https://news.ycombinator.com/?p={page_number}")
+    page_soup = BeautifulSoup(page_res.text, "html.parser")
+    page_links = page_soup.select(".titleline")
+    page_subtext = page_soup.select(".subtext")
+
+    links = links + page_links
+    subtext = subtext + page_subtext
+
+
+def sort_stories_by_votes(hn_list):
+    return sorted(hn_list, key=lambda k:k['votes'], reverse=True)
 
 
 def create_custom_hn(links, subtext):
@@ -19,7 +33,7 @@ def create_custom_hn(links, subtext):
             points = int(vote[0].getText().replace(" points", ""))
             if points >= 100:
                 hn.append({'title': title, 'href': href, 'votes': points})
-    return hn
+    return sort_stories_by_votes(hn)
 
 
 pprint(create_custom_hn(links, subtext))
